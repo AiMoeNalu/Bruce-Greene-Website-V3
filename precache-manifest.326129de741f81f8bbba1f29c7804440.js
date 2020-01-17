51e64419b7adca93a3e69552a8a91bee2ed30674
AiMoeNalu <48229683+AiMoeNalu@users.noreply.github.com>
2020-01-17 14:12:59 -0500

    Updates

---
+++ precache-manifest.326129de741f81f8bbba1f29c7804440.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c40d8b3da9ffcafc767e5bfe5f166a1",
+    "url": "/Bruce-Greene-Website-V3/index.html"
+  },
+  {
+    "revision": "a1f963bdc3dc6a7d9256",
+    "url": "/Bruce-Greene-Website-V3/static/css/main.b0b1dd11.chunk.css"
+  },
+  {
+    "revision": "cf535efd09d31a25637b",
+    "url": "/Bruce-Greene-Website-V3/static/js/2.28a09539.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Bruce-Greene-Website-V3/static/js/2.28a09539.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1f963bdc3dc6a7d9256",
+    "url": "/Bruce-Greene-Website-V3/static/js/main.d71165c3.chunk.js"
+  },
+  {
+    "revision": "f0f1d75ced8bb47e68e1",
+    "url": "/Bruce-Greene-Website-V3/static/js/runtime-main.f682794b.js"
+  },
+  {
+    "revision": "c9b542c91e8ef706f135ea9aea3a7fe1",
+    "url": "/Bruce-Greene-Website-V3/static/media/Instagram.c9b542c9.svg"
+  },
+  {
+    "revision": "734cc0b70c27cfc48f670606f0a6f6b7",
+    "url": "/Bruce-Greene-Website-V3/static/media/Twitch.734cc0b7.svg"
+  },
+  {
+    "revision": "6c2a8839e4306e736a5062ace5a3c9b5",
+    "url": "/Bruce-Greene-Website-V3/static/media/Twitter.6c2a8839.svg"
+  },
+  {
+    "revision": "026b08eefc8360273cf5441f15691c20",
+    "url": "/Bruce-Greene-Website-V3/static/media/VibrantGreenLogo.026b08ee.svg"
+  },
+  {
+    "revision": "0e9234f1b35379a15daf52875ff11d06",
+    "url": "/Bruce-Greene-Website-V3/static/media/Youtube.0e9234f1.svg"
+  }
+]);
